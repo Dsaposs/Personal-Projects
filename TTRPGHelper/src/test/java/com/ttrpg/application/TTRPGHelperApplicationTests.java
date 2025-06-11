c2deb428b5e5c0e9f47a931fe0ce589e7d35fa66
Dan Sapossnek <dsaposs@hotmail.com>
2025-06-11 16:07:16 -0400

    Fixed failing unit tests

--- TTRPGHelper/src/test/java/com/ttrpg/application/TTRPGHelperApplicationTests.java
+++ TTRPGHelper/src/test/java/com/ttrpg/application/TTRPGHelperApplicationTests.java
@@ -2,17 +2,18 @@ package com.ttrpg.application;
 
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
 
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT;
@@ -22,36 +23,42 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 class TTRPGHelperApplicationTests {
     TestRestTemplate restTemplate;
     URL base;
-//    @LocalServerPort
+    @LocalServerPort
     int port = 8081;
 
-    @BeforeEach
-    public void setUp() throws MalformedURLException {
-        restTemplate = new TestRestTemplate("foo", "foo1");
-        base = new URL("http://localhost:" + port + "/hello");
+    @Autowired
+    UsersRepository usersRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    public void setUpUser() {
+        usersRepository.deleteAll();
+        User newUser = new User();
+        newUser.setUsername("newUser");
+        newUser.setPassword(passwordEncoder.encode("newPassword"));
+        usersRepository.save(newUser);
     }
 
     @Test
-    public void whenLoggedUserRequestsHomePage_ThenSuccess()
+    public void testFailBasicAuthentication()
             throws IllegalStateException, IOException {
-        ResponseEntity<String> response =
-                restTemplate.getForEntity(base.toString(), String.class);
-
-        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
-        Assertions.assertNotNull(response.getBody());
-        Assertions.assertTrue(response.getBody().contains("Baeldung"));
-    }
-
-    @Test
-    public void whenUserWithWrongCredentials_thenUnauthorizedPage()
-            throws Exception {
-
-        restTemplate = new TestRestTemplate("user", "wrongpassword");
+        restTemplate = new TestRestTemplate("foo", "foo1");
+        base = new URL("http://localhost:" + port + "/auth/all");
         ResponseEntity<String> response =
                 restTemplate.getForEntity(base.toString(), String.class);
 
         Assertions.assertEquals(HttpStatus.UNAUTHORIZED, response.getStatusCode());
-        Assertions.assertNotNull(response.getBody());
-        Assertions.assertTrue(response.getBody().contains("Unauthorized"));
+    }
+
+    @Test
+    public void testPassBasicAuthentication()
+            throws Exception {
+        setUpUser();
+        restTemplate = new TestRestTemplate("newUser","newPassword");
+        base = new URL("http://localhost:" + port + "/auth/all");
+        ResponseEntity<String> response = restTemplate.getForEntity(base.toString(), String.class);
+
+        Assertions.assertEquals(HttpStatus.OK, response.getStatusCode());
     }
 }
