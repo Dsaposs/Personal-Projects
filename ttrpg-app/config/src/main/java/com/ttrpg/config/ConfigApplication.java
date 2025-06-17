2931f7bd36e5abe3de460c36649c3edc86c6ff1d
Dan Sapossnek <dsaposs@hotmail.com>
2025-06-17 13:49:40 -0400

    config server added

--- ttrpg-app/config/src/main/java/com/ttrpg/config/ConfigApplication.java
+++ ttrpg-app/config/src/main/java/com/ttrpg/config/ConfigApplication.java
@@ -1,17 +1,17 @@
-package com.ttrpg.auth;
+package com.ttrpg.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EntityScan("com.ttrpg.helper")
-public class AuthApplication {
+@EnableConfigServer
+public class ConfigApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AuthApplication.class, args);
+        SpringApplication.run(ConfigApplication.class, args);
     }
 
 }
