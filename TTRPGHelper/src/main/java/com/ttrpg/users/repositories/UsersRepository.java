5107a5bfe6f24ea4e66665cb441c91bfd6b041bc
Dan Sapossnek <dsaposs@hotmail.com>
2025-06-18 12:29:52 -0400

    moved git parent directory

--- TTRPGHelper/src/main/java/com/ttrpg/users/repositories/UsersRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ttrpg.users.repositories;
-
-import com.ttrpg.users.repositories.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsersRepository extends JpaRepository<User,Integer> {
-}
--- TTRPGHelper/src/main/resources/MySQL/TTRPG-Helper-DB.sql
+++
@@ -1,3 +0,0 @@
-DROP DATABASE ttrpg_helper_db;
-CREATE DATABASE ttrpg_helper_db;
-USE ttrpg_helper_db;
